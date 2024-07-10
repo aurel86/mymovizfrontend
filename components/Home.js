641c97ea47a0195bd02fbb969d4cef2294aba4ba
Aurelien <a.sanctorum1301@gmail.com>
2024-07-10 16:02:55 +0200

    mymovizfrontend

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://mymovizbackend-red.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
         setMoviesData(data.movies);
